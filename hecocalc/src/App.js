65dc3555b7a5f975d3a74acd95ad44c608c34006
elainatesfai <elaina.tesfai@gmail.com>
2023-01-22 15:05:55 +0000

    More changes (a lot to be fixed)
    dropdown menu functionality started -> alot to be added...

--- hecocalc/src/App.js
+++ hecocalc/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
+    // <Router>
+    //    <Routes>
+    //       <Route path='/' element={<Home />} />
+    //   </Routes>
+    // </Router>
+
     <>
       <Navbar />
     </>
--- hecocalc/src/components/Navbar.jsx
+++ hecocalc/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState } from "react";
 import '../css/navbar.css';
 
-function Navbar() {
+
+export default function Navbar() {
+
+ // to change burger classes
+ const setBurgerClass = useState("BurgerBar unclicked")
+ const setMenuClass = useState("DropdownMenu hidden")
+ const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+ // toggle burger DropdownMenu change
+ const updateMenu = () => {
+   if(!isMenuClicked) {
+     setBurgerClass('BurgerBar clicked')
+     setMenuClass('DropdownMenu visible')
+   }
+   else {
+     setBurgerClass('BurgerBar unclicked')
+     setMenuClass('DropdownMenu hidden')
+   }
+ }
+
   return (
+    <>
     <div className='Nav'>
       <div className='NavContainer'>
         {/* This is the div that contains the navbar logo */}
@@ -10,11 +30,20 @@ function Navbar() {
             <img className='NavIcon' src='./assets/hecologo.png' />
         </div>
 
+        <div className='MobileIcon' onClick={() => updateMenu(setIsMenuClicked(!isMenuClicked))}>
+          {(isMenuClicked?<i className='fas fa-xmark'/>:<i className='fas fa-bars-staggered'/>)}
+        </div>
+
+        {/* <div className='MobileIcon'>
+         <i className='fas fa-bars-staggered'/>
+        </div> */}
+
         {/* This div holds all the items in the navbar menu */}
         <div className='NavMenu'>
           <div className='NavItem'>
             <div className='NavLink'>
               Inputs
+              {/* <i className='fas fa-caret-right' /> */}
             </div>
           </div>
           <div className='NavItem'>
@@ -25,6 +54,7 @@ function Navbar() {
           <div className='NavItem'>
             <div className='NavLink'>
               Model
+              {/* <i className='fas fa-caret-right' /> */}
             </div>
           </div>
         </div>
@@ -44,8 +74,24 @@ function Navbar() {
         </div>
 
       </div>
+      
     </div>
+    <div className={(isMenuClicked?'DropdownMenu':'DropdownMenuHidden')}>
+        <div className='DropdownMenuList'>
+          <div className='DropdownMenuItem'>
+            Inputs
+          </div>
+          <div className='DropdownMenuItem'>
+            Results
+          </div>
+          <div className='DropdownMenuItem'>
+            Model
+          </div>
+          <div className='DropdownMenuItem'>
+            Team-based Learning
+          </div>
+        </div>
+     </div>
+  </>
   )
 }
-
-export default Navbar
