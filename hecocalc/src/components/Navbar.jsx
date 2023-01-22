18c68bb4fd6a9a5fe15eeec78dec24d403a33754
elainatesfai <elaina.tesfai@gmail.com>
2023-01-22 00:21:37 +0000

    Minor Updates
    Some changes on the index.html, and more iterations to the navbar component -> both CSS and JSX.

--- hecocalc/src/components/Navbar.jsx
+++ hecocalc/src/components/Navbar.jsx
@@ -5,9 +5,44 @@ function Navbar() {
   return (
     <div className='Nav'>
       <div className='NavContainer'>
+        {/* This is the div that contains the navbar logo */}
         <div className='NavLogo'>
             <img className='NavIcon' src='./assets/hecologo.png' />
         </div>
+
+        {/* This div holds all the items in the navbar menu */}
+        <div className='NavMenu'>
+          <div className='NavItem'>
+            <div className='NavLink'>
+              Inputs
+            </div>
+          </div>
+          <div className='NavItem'>
+            <div className='NavLink'>
+              Results
+            </div>
+          </div>
+          <div className='NavItem'>
+            <div className='NavLink'>
+              Model
+            </div>
+          </div>
+        </div>
+
+        {/* This is the div that contains the the buttons on the navbar */}
+        <div className='NavBtnContainer'>
+          <div className='NavBtn'>
+            <div className='NavBtnLink'>
+              Signup
+            </div>
+          </div>
+          <div className='NavBtn'>
+            <div className='NavBtnLink'>
+              Login
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   )
