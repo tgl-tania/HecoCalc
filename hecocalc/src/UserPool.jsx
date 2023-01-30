22079c38ad9ac96cb5edac044fdeb35e72c9936e
Khin Khin Htet <chanchan@MacBook-Air.local>
2023-01-30 00:44:58 +0000

    code for user login

--- hecocalc/src/UserPool.jsx
+++ hecocalc/src/UserPool.jsx
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-    UserPoolId: 'eu-west-2_dGzk6PESE',
-    ClientId: '4cp4uuo3dtp2l2me4oj3abvbpt'
+    UserPoolId: 'eu-west-2_D4H1dwVyg',
+    ClientId: '3ggh8416n0aqt4o415qgm40376'
 };
 
 export default new CognitoUserPool(poolData);
