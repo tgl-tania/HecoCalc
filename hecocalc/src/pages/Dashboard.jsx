bfde95560ef9cc4e183617940435d40bd8a54c59
Khin Khin Htet <1944206@brunel.ac.uk>
2023-02-01 13:24:46 +0000

    Update Dashboard.jsx

--- hecocalc/src/pages/Dashboard.jsx
+++ hecocalc/src/pages/Dashboard.jsx
@@ -1,10 +1,20 @@
 import Linechart from "../components/Linechart";
+import ScatterPlot from "../components/ScatterPlot";
+import Tornado from "../components/TornadoDiagram";
 
 function GraphsDashboard() {
   return (
+    <>
     <div className="LineChart">
         <Linechart/>
     </div>
+    <div className="ScatterPlot">
+      <ScatterPlot/>
+    </div>
+    <div className="TornadoDiagram">
+      <Tornado/>
+    </div>
+  </>
   );
 }
 
