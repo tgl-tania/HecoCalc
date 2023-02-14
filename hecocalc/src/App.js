0f31993202fbf6bd7e33773250f9d43863dd70c2
elainatesfai <elaina.tesfai@gmail.com>
2023-02-14 01:38:54 +0000

    Removing uneccessary files

--- hecocalc/src/App.js
+++ hecocalc/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import Home from './pages/Home';
 import Login from './pages/Login';
 import Results from './pages/Results';
 
@@ -11,10 +10,9 @@ function App() {
   return (
     <Router>
        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/results' element={<Results />}/>
+          <Route path='/' element={<Login />}/>
           <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/results' element={<Results />}/>
       </Routes>
     </Router>
   );
