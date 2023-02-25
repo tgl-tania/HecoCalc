7eb9446ef202b83c12ee853f8638e8e72e16f36c
LaurenRuuyx <98824446+LaurenRuuyx@users.noreply.github.com>
2023-02-25 22:14:45 +0000

    added repository selection page
    Important:
    Changed the routes into AppRouter.js
    This was done in order to use the outlet context inside App.js

--- hecocalc/src/pages/SettingsPage.jsx
+++ hecocalc/src/pages/SettingsPage.jsx
@@ -1,12 +1,43 @@
+import { useOutletContext } from "react-router-dom";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "../UserPool";
+import "../css/loginsettings.css";
+
+
 
 
 
 export default function Settings(){
-    
 
+    const getRepo = useOutletContext().getRepo;
+    var repositories = getRepo();
+    console.log(repositories);
+    var choices = [];
+    repositories.map(item => {
+        let arr = item.Key.split('/');
+        if(arr.length>0){
+            if(choices.includes(arr[1])===false){
+                choices.push(arr[1]);
+            }
+        }
+    })
+
+    
     return(
         <div>
-            <h1>HelloWorld</h1>
+            <h1>Choose Repository</h1>
+            <br></br>
+            <div className="login-settings-form-div">
+            <form>
+                <select>
+                {choices.map(item => {
+                return(
+                    <option value={item}>{item}</option>
+                )
+            })}
+                </select>
+            </form>
+            </div>
         </div>
     );
 }
